6100e0559ed31194d3582acc795dba437b5c4e72
Jefferson Miranda <jefferson472@hotmail.com>
2023-04-16 08:50:45 -0300

    refact: alterado para modo class

--- 15-projeto-plataforma-de-jogo/Lava.js
+++ 15-projeto-plataforma-de-jogo/Lava.js
@@ -1,25 +1,27 @@
-function Lava(pos, ch) {
-  this.pos = pos;
-  this.size = new Vector(1, 1);
+class Lava {
+  constructor(pos, ch) {
+    this.pos = pos;
+    this.size = new Vector(1, 1);
+    this.type = "lava";
 
-  if (ch == "=")
-    this.speed = new Vector(2, 0);
-  else if (ch == "|")
-    this.speed = new Vector(0, 2);
-  else if (ch == "v") {
-    this.speed = new Vector(0, 3);
-    this.repeatPos = pos;
+    if (ch == "=")
+      this.speed = new Vector(2, 0);
+    else if (ch == "|")
+      this.speed = new Vector(0, 2);
+    else if (ch == "v") {
+      this.speed = new Vector(0, 3);
+      this.repeatPos = pos;
+    }
+  }
+
+  act(step, level) {
+    var newPos = this.pos.plus(this.speed.times(step));
+    if (!level.obstacleAt(newPos, this.size))
+      this.pos = newPos;
+    else if (this.repeatPos)
+      this.pos = this.repeatPos;
+
+    else
+      this.speed = this.speed.times(-1);
   }
 }
-
-Lava.prototype.type = "lava";
-
-Lava.prototype.act = function (step, level) {
-  var newPos = this.pos.plus(this.speed.times(step));
-  if (!level.obstacleAt(newPos, this.size))
-    this.pos = newPos;
-  else if (this.repeatPos)
-    this.pos = this.repeatPos;
-  else
-    this.speed = this.speed.times(-1);
-};
