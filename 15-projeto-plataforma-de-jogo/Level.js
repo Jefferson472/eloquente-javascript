1b321cc8f698c79f16977e7e68fa1296c24b3d3d
Jefferson Miranda <jefferson472@hotmail.com>
2023-04-16 10:56:58 -0300

    feat: add player touched

--- 15-projeto-plataforma-de-jogo/Level.js
+++ 15-projeto-plataforma-de-jogo/Level.js
@@ -1,12 +1,25 @@
-function Level(plan) {
-    this.width = plan[0].lenght;
-    this.height = plan[0].lenght;
+import {Coin} from "./Coin.js"
+import {Lava} from "./Lava.js"
+import {Player} from "./Player.js"
+import {Vector} from "./Vector.js"
+
+
+class Level {
+  constructor(plan) {
+    this.width = plan[0].length;
+    this.height = plan.length;
     this.grid = [];
     this.actors = [];
-  
+
+    var actorChars = {
+      "@": Player,
+      "o": Coin,
+      "=": Lava, "|": Lava, "v": Lava,
+    }
+
     for (var y = 0; y < this.height; y++) {
       var line = plan[y], gridLine = [];
-  
+
       for (var x = 0; x < this.width; x++) {
         var ch = line[x], fieldType = null;
         var Actor = actorChars[ch];
@@ -18,24 +31,25 @@ function Level(plan) {
           fieldType = "lava";
         gridLine.push(fieldType);
       }
-  
-      this.player = this.actors.filter(function(actor) {
-        return actor.type == "player";
-      })[0];
-      this.status = this.finishDelay = null;
+      this.grid.push(gridLine);
     }
+  
+    this.player = this.actors.filter(function (actor) {
+      return actor.type == "player";
+    })[0];
+    this.status = this.finishDelay = null;
   }
 
-  Level.prototype.isFinished = function() {
+  isFinished() {
     return this.status != null && this.finishDelay < 0;
-  };
+  }
 
-  Level.prototype.obstacleAt = function(pos, size) {
+  obstacleAt(pos, size) {
     var xStart = Math.floor(pos.x);
     var xEnd = Math.ceil(pos.x + size.x);
     var yStart = Math.floor(pos.y);
     var yEnd = Math.ceil(pos.y + size.y);
-  
+
     if (xStart < 0 || xEnd > this.width || yStart < 0)
       return "wall";
     if (yEnd > this.height)
@@ -47,34 +61,50 @@ function Level(plan) {
           return fieldType;
       }
     }
-  };
-  
-  
-  Level.prototype.actorAt = function(actor) { 
+  }
+
+  actorAt(actor) {
     for (var i = 0; i < this.actors.length; i++) {
       var other = this.actors[i];
-      if (
-        actor.pos.x + actor.size.x > other.pos.x &&
+      if (actor.pos.x + actor.size.x > other.pos.x &&
         actor.pos.x < other.pos.x + other.size.x &&
         actor.pos.y + actor.size.y > other.pos.y &&
-        actor.pos.y < other.pos.y + other.size.y
-      )
-      return other;
+        actor.pos.y < other.pos.y + other.size.y)
+        return other;
     }
   }
-  
-  
-  var maxStep = 0.05;
-  
-  Level.prototype.animate = function(step, keys) {
+
+  animate(step, keys) {
+    var maxStep = 0.05;
+
     if (this.status != null)
       this.finishDelay -= step;
-  
+
     while (step > 0) {
       var thisStep = Math.min(step, maxStep);
-      this.actors.forEach(function(actor) {
+      this.actors.forEach(function (actor) {
         actor.act(thisStep, this, keys);
       }, this);
       step -= thisStep;
     }
+  }
+
+  playerTouched = function(type, actor) {
+    if (type == "lava" && this.status == null) {
+      this.status = "lost";
+      this.finishDelay = 1;
+    } else if (type == "coin") {
+      this.actors = this.actors.filter(function(other) {
+        return other != actor;
+      });
+      if (!this.actors.some(function(actor) {
+        return actor.type == "coin";
+      })) {
+        this.status = "won";
+        this.finishDelay = 1;
+      }
+    }
   };
+}
+
+export {Level}
