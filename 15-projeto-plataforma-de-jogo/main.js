9bad4fc45dd4dc94ed4caeb8da263670d020b8e8
Jefferson Miranda <jefferson472@hotmail.com>
2023-04-16 10:58:54 -0300

    fix: alterando objetos para módulos

--- 15-projeto-plataforma-de-jogo/main.js
+++ 15-projeto-plataforma-de-jogo/main.js
@@ -1,4 +1,9 @@
-var simpleLevelPlan = [
+import {DOMDisplay} from "./DOMDisplay.js"
+import {Level} from "./Level.js"
+import {trackKeys} from "./utils.js"
+
+
+var plan = [
   "                      ",
   "                      ",
   "  x              = x  ",
@@ -10,50 +15,52 @@ var simpleLevelPlan = [
   "                      "
 ];
 
+var GAME_LEVELS = [plan]
+var arrowCodes = {37: "left", 38: "up", 39: "right"};
+var arrows = trackKeys(arrowCodes);
 
-var actorChars = {
-  "@": Player,
-  "o": Coin,
-  "=": Lava, "|": Lava, "v": Lava,
+runGame(GAME_LEVELS, DOMDisplay);
+
+
+function runGame(plans, Display) {
+  function startLevel(n) {
+    runLevel(new Level(plans[n]), Display, function(status) {
+      if (status == "lost")
+        startLevel(n);
+      else if (n < plans.length - 1)
+        startLevel(n + 1);
+      else
+        console.log("You win!");
+    });
+  }
+  startLevel(0);
 }
 
-
-function Vector(x, y) {
-  this.x = x; this.y = y;
-};
-
-Vector.prototype.plus = function(other) {
-  return new Vector(this.x + other.x, this.y + other.y);
-};
-
-Vector.prototype.times = function(factor) {
-  return new Vector(this.x * factor, this.y * factor);
-};
-
-
-function Player(pos) {
-  this.pos = pos.plus(new Vector(0, -0.5));
-  this.size = new Vector(0.8, 1.5);
-  this.speed = new Vector(0, 0);
+function runLevel(level, Display, andThen) {
+  var display = new Display(document.body, level);
+  runAnimation(function(step) {
+    level.animate(step, arrows);
+    display.drawFrame(step);
+    if (level.isFinished()) {
+      display.clear();
+      if (andThen)
+        andThen(level.status);
+      return false;
+    }
+  });
 }
 
-Player.prototype.type = "player";
-
-
-function Coin(pos) {
-  this.basePos = this.pos = pos.plus(new Vector(0.2, 0.1));
-  this.size = new Vector(0.6, 0.6);
-  this.wobble = Math.random() * Math.PI * 2;
-}
-
-Coin.prototype.type = "coin";
-
-
-var simpleLevel = new Level(simpleLevelPlan);
-console.log(simpleLevel.width, "by", simpleLevel.height);
-
-function elt(name, className) {
-  var elt = document.createElement(name);
-  if (className) elt.className = className;
-  return elt;
+function runAnimation(frameFunc) {
+  var lastTime = null;
+  function frame(time) {
+    var stop = false;
+    if (lastTime != null) {
+      var timeStep = Math.min(time - lastTime, 100) / 1000;
+      stop = frameFunc(timeStep) === false;
+    }
+    lastTime = time;
+    if (!stop)
+      requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
 }
