be9c3035e21040b348f3603f557f53fc86589c04
Jefferson Miranda <jefferson472@hotmail.com>
2023-04-16 11:07:53 -0300

    refact: movendo functions para pasta utils

--- 15-projeto-plataforma-de-jogo/main.js
+++ 15-projeto-plataforma-de-jogo/main.js
@@ -1,6 +1,5 @@
 import {DOMDisplay} from "./components/DOMDisplay.js"
-import {Level} from "./components/Level.js"
-import {trackKeys} from "./utils.js"
+import {runGame} from "./utils/runGame.js"
 
 
 var plan = [
@@ -16,51 +15,5 @@ var plan = [
 ];
 
 var GAME_LEVELS = [plan]
-var arrowCodes = {37: "left", 38: "up", 39: "right"};
-var arrows = trackKeys(arrowCodes);
 
 runGame(GAME_LEVELS, DOMDisplay);
-
-
-function runGame(plans, Display) {
-  function startLevel(n) {
-    runLevel(new Level(plans[n]), Display, function(status) {
-      if (status == "lost")
-        startLevel(n);
-      else if (n < plans.length - 1)
-        startLevel(n + 1);
-      else
-        console.log("You win!");
-    });
-  }
-  startLevel(0);
-}
-
-function runLevel(level, Display, andThen) {
-  var display = new Display(document.body, level);
-  runAnimation(function(step) {
-    level.animate(step, arrows);
-    display.drawFrame(step);
-    if (level.isFinished()) {
-      display.clear();
-      if (andThen)
-        andThen(level.status);
-      return false;
-    }
-  });
-}
-
-function runAnimation(frameFunc) {
-  var lastTime = null;
-  function frame(time) {
-    var stop = false;
-    if (lastTime != null) {
-      var timeStep = Math.min(time - lastTime, 100) / 1000;
-      stop = frameFunc(timeStep) === false;
-    }
-    lastTime = time;
-    if (!stop)
-      requestAnimationFrame(frame);
-  }
-  requestAnimationFrame(frame);
-}
---
+++ 15-projeto-plataforma-de-jogo/utils/runGame.js
@@ -0,0 +1,54 @@
+import {Level} from "../components/Level.js"
+import {trackKeys} from "./trackKeys.js"
+
+
+var arrowCodes = {37: "left", 38: "up", 39: "right"};
+var arrows = trackKeys(arrowCodes);
+
+function runGame(plans, Display) {
+  function startLevel(n) {
+    runLevel(new Level(plans[n]), Display, function(status) {
+      if (status == "lost")
+        startLevel(n);
+      else if (n < plans.length - 1)
+        startLevel(n + 1);
+      else
+        console.log("You win!");
+    });
+  }
+  startLevel(0);
+}
+
+function runLevel(level, Display, andThen) {
+  var display = new Display(document.body, level);
+  runAnimation(function(step) {
+    level.animate(step, arrows);
+    display.drawFrame(step);
+    if (level.isFinished()) {
+      display.clear();
+      if (andThen)
+        andThen(level.status);
+      return false;
+    }
+  });
+}
+
+function runAnimation(frameFunc) {
+  var lastTime = null;
+  function frame(time) {
+    var stop = false;
+    if (lastTime != null) {
+      var timeStep = Math.min(time - lastTime, 100) / 1000;
+      stop = frameFunc(timeStep) === false;
+    }
+    lastTime = time;
+    if (!stop)
+      requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
+}
+
+
+export {runAnimation}
+export {runGame}
+export {runLevel}
