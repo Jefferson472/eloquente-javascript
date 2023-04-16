2ed80318fec682f9143722a1180621567099b618
Jefferson Miranda <jefferson472@hotmail.com>
2023-04-16 10:56:15 -0300

    fix: class DOMDisplay

--- 15-projeto-plataforma-de-jogo/DOMDisplay.js
+++ 15-projeto-plataforma-de-jogo/DOMDisplay.js
@@ -1,63 +1,57 @@
-function DOMDisplay(parent, level) {
+class DOMDisplay {
+  constructor(parent, level) {
     this.wrap = parent.appendChild(elt("div", "game"));
     this.level = level;
-  
     this.wrap.appendChild(this.drawBackground());
     this.actorLayer = null;
     this.drawFrame();
   }
-  
-  
-  var scale = 20;
-  
-  DOMDisplay.prototype.drawBackground = function() {
+
+  drawBackground() {
     var table = elt("table", "background");
     table.style.width = this.level.width * scale + "px";
-    this.level.grid.forEach(function(row) {
+    this.level.grid.forEach(function (row) {
       var rowElt = table.appendChild(elt("tr"));
       rowElt.style.height = scale + "px";
-      row.forEach(function(type) {
+      row.forEach(function (type) {
         rowElt.appendChild(elt("td", type));
       });
     });
     return table;
   }
-  
-  
-  DOMDisplay.prototype.drawActors = function() {
+
+  drawActors = function() {
     var wrap = elt("div");
     this.level.actors.forEach(function(actor) {
       var rect = wrap.appendChild(elt("div", "actor " + actor.type));
-      rect.stye.width = actor.size.x * scale + "px";
-      rect.stye.height = actor.size.y * scale + "px";
-      rect.stye.left = actor.pos.x * scale + "px";
-      rect.stye.top = actor.pos.y * scale + "px";
+      rect.style.width = actor.size.x * scale + "px";
+      rect.style.height = actor.size.y * scale + "px";
+      rect.style.left = actor.pos.x * scale + "px";
+      rect.style.top = actor.pos.y * scale + "px";
     });
     return wrap;
-  }
-  
-  
-  DOMDisplay.prototype.drawFrame = function() {
+  };
+
+  drawFrame() {
     if (this.actorLayer)
       this.wrap.removeChild(this.actorLayer);
     this.actorLayer = this.wrap.appendChild(this.drawActors());
     this.wrap.className = "game " + (this.level.status || "");
     this.scrollPlayerIntoView();
   }
-  
-  
-  DOMDisplay.prototype.scrollPlayerIntoView = function() {
+
+  scrollPlayerIntoView() {
     var width = this.wrap.clientWidth;
     var height = this.wrap.clientHeight;
     var margin = width / 3;
-  
+
     // The viewport
     var left = this.wrap.scrollLeft, right = left + width;
     var top = this.wrap.scrollTop, bottom = top + height;
-  
+
     var player = this.level.player;
     var center = player.pos.plus(player.size.times(0.5)).times(scale);
-  
+
     if (center.x < left + margin)
       this.wrap.scrollLeft = center.x - margin;
     else if (center.x > right - margin)
@@ -66,11 +60,20 @@ function DOMDisplay(parent, level) {
       this.wrap.scrollTop = center.y - margin;
     else if (center.y > bottom - margin)
       this.wrap.scrollTop = center.y + margin - height;
-  };
-  
-  
-  DOMDisplay.prototype.clear = function() {
+  }
+
+  clear() {
     this.wrap.parentNode.removeChild(this.wrap);
-  };
-  
-  
+  }
+
+}
+
+var scale = 20;
+
+function elt(name, className) {
+  var elt = document.createElement(name);
+  if (className) elt.className = className;
+  return elt;
+}
+
+export {DOMDisplay}
